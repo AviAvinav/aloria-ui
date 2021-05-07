d8f515823a52b7a207899ef08bf8f08ac41912ce
Avi Avinav <aviavinav2004@gmail.com>
2021-05-07 20:02:33 +0530

    Added new exports

--- src/index.js
+++ src/index.js
@@ -4,3 +4,4 @@ export { default as useLoading } from "./hooks/useLoading";
 export { default as Button } from "./components/Button";
 export { default as Input } from "./components/Input";
 export { default as Label } from "./components/Label";
+export { default as Form } from "./components/Form";
