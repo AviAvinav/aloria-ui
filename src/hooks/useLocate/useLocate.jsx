9d0fae6ee2b3304447613c42413df730a6e5aad0
Avi Avinav <aviavinav2004@gmail.com>
2021-05-07 18:53:41 +0530

    Updated useLocate

--- src/hooks/useLocate/useLocate.jsx
+++ src/hooks/useLocate/useLocate.jsx
@@ -1,9 +1,16 @@
 import React, { useState, useEffect } from "react";
 
-export const useLocate = (args) => {
-  const [value, setValue] = useState(false);
-
-  useEffect(() => {}, [args]);
+export const useLocate = () => {
+  const [value, setValue] = useState();
+  const failure = () => "error";
+  function success(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    return latitude, longitude;
+  }
+  useEffect(() => {
+    setValue(navigator.geolocation.getCurrentPosition(success, failure));
+  }, [success, failure]);
 
   return [value];
 };
