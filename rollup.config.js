be63feaa6506710afc8051345343b7647cda689c
Avi Avinav <aviavinav2004@gmail.com>
2021-05-07 21:26:56 +0530

    command for rollup added

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import babel from "rollup-plugin-babel";
 import resolve from "@rollup/plugin-node-resolve";
 import external from "rollup-plugin-peer-deps-external";
+import postcss from "rollup-plugin-postcss";
 
 export default [
   {
@@ -17,6 +18,10 @@ export default [
       },
     ],
     plugins: [
+      postcss({
+        plugins: [],
+        minimize: true,
+      }),
       babel({
         exclude: "node_modules/**",
         presets: ["@babel/preset-react"],
