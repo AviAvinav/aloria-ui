2bc823f721c4b8b647f43ad07c9d0de9a66cb3f7
Avi Avinav <aviavinav2004@gmail.com>
2021-05-07 21:59:14 +0530

    Fixed files for packaging

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-export { default as Card } from "./components/Card";
-export { default as Container } from "./components/Container";
-export { default as useLoading } from "./hooks/useLoading";
-export { default as Button } from "./components/Button";
-export { default as Input } from "./components/Input";
-export { default as Label } from "./components/Label";
-export { default as Form } from "./components/Form";
+export { Card } from "./components/Card";
+export { Container } from "./components/Container";
+export { useLoading } from "./hooks/useLoading";
+export { Button } from "./components/Button";
+export { Input } from "./components/Input";
+export { Label } from "./components/Label";
+export { Form } from "./components/Form";
