cfb35f8853bc60c88914b75d3132e09a720d5ecb
Avi Avinav <aviavinav2004@gmail.com>
2021-05-08 05:27:38 +0530

    Updated useHovered

--- src/hooks/useHovered/useHovered.js
+++ src/hooks/useHovered/useHovered.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 export const useHovered = (args) => {
+  const args = useRef();
   const [value, setValue] = useState(false);
   const toggleHover = () => {
     setHovered(!hovered);
