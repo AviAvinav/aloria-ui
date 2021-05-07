cc56fbb25b799e68852cf5562ba44c25ff699da9
Avi Avinav <aviavinav2004@gmail.com>
2021-05-08 03:43:17 +0530

    Added FormInput to the exports

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ export { useLoading } from "./hooks/useLoading";
 export { Button } from "./components/Button";
 export { Input } from "./components/Input";
 export { Label } from "./components/Label";
-export { Form } from "./components/Form";
+export { Form, FormInput } from "./components/Form";
